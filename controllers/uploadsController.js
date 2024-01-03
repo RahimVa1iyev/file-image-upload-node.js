9dedea2b05f0c99d51b00ab3235aa32237c8040b
rahimvaliyev <rahimvaliyev99@gmail.com>
2024-01-03 16:03:51 +0400

    error-handler-in-upload-controller

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,16 +1,33 @@
-const {StatusCodes} = require('http-status-codes')
+const { StatusCodes } = require('http-status-codes')
 const path = require('path')
+const { BadRequestError } = require('../errors')
 
-const uploadProductImage = async (req,res) =>{
+const uploadProductImage = async (req, res) => {
+
+    if (!req.files) {
+        throw new BadRequestError('Uploads image file')
+    }
+
     let productImage = req.files.image
     
-    const imagePath = path.join(__dirname ,'../public/uploads/'+`${productImage.name}`)
+    if (!productImage.mimetype.startsWith('image')) {
+        throw new BadRequestError('Uploads image file')
+    }
+
+    const size = 1024 * 1024
+
+    if (productImage.size > 1000) {
+        throw new BadRequestError(`Image file smaller than ${size}`)
+    }
+
+
+    const imagePath = path.join(__dirname, '../public/uploads/' + `${productImage.name}`)
 
     await productImage.mv(imagePath)
-    res.status(StatusCodes.CREATED).json({image : {src: `/uploads/${productImage.name}`}})
+    res.status(StatusCodes.CREATED).json({ image: { src: `/uploads/${productImage.name}` } })
 
 }
 
 
-module.exports = {uploadProductImage}
+module.exports = { uploadProductImage }
 
