80839271c46604b81640c1cc8ed217f832af6f9a
rahimvaliyev <rahimvaliyev99@gmail.com>
2024-01-03 15:52:55 +0400

    upload-image-controller

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,8 +1,14 @@
-const Product = require('../models/Product')
 const {StatusCodes} = require('http-status-codes')
+const path = require('path')
 
-const uploadProductImage = async(req,res) =>{
-    res.send("create product")
+const uploadProductImage = async (req,res) =>{
+    let productImage = req.files.image
+    
+    const imagePath = path.join(__dirname ,'../public/uploads/'+`${productImage.name}`)
+
+    await productImage.mv(imagePath)
+    res.status(StatusCodes.CREATED).json({image : {src: `/uploads/${productImage.name}`}})
+
 }
 
 
