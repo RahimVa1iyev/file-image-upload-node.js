a038c366cdad1788d0229d76c6e045dccd94c1c0
rahimvaliyev <rahimvaliyev99@gmail.com>
2024-01-03 16:56:02 +0400

    send-image-to-cloudinary

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const fileUpload = require('express-fileupload')
 const cloudinary = require('cloudinary').v2
 cloudinary.config({
   cloud_name : process.env.CLOUD_NAME,
-  cloud_key : process.env.CLOUD_API_KEY,
-  cloud_secret : process.env.CLOUD_API_SECRET
+  api_key : process.env.CLOUD_API_KEY,
+  api_secret : process.env.CLOUD_API_SECRET
 })
 
 // database
@@ -21,8 +21,9 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.static('./public'))
+
 app.use(express.json())
-app.use(fileUpload())
+app.use(fileUpload({useTempFiles:true}))
 
 app.get('/', (req, res) => {
   res.send('<h1>File Upload Starter</h1>');
--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,8 +1,11 @@
 const { StatusCodes } = require('http-status-codes')
 const path = require('path')
 const { BadRequestError } = require('../errors')
+const cloudinary = require('cloudinary').v2;
+const fs = require('fs');
 
-const uploadProductImage = async (req, res) => {
+
+const uploadProductImageLoacl = async (req, res) => {
 
     if (!req.files) {
         throw new BadRequestError('Uploads image file')
@@ -25,6 +28,19 @@ const uploadProductImage = async (req, res) => {
 
     await productImage.mv(imagePath)
     res.status(StatusCodes.CREATED).json({ image: { src: `/uploads/${productImage.name}` } })
+
+}
+
+const uploadProductImage = async (req, res) => {
+    const result = await cloudinary.uploader.upload(
+        req.files.image.tempFilePath,
+        {
+          use_filename: true,
+          folder: 'upload-file',
+        }
+      );
+      fs.unlinkSync(req.files.image.tempFilePath);
+      return res.status(StatusCodes.OK).json({ image: { src: result.secure_url } });
 
 }
 
