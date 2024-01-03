d96d3647e70e09892778d276d10e3e6547262009
rahimvaliyev <rahimvaliyev99@gmail.com>
2024-01-03 15:07:07 +0400

    setup-router

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+const productRouter = require('./routes/productRoutes')
+
 // database
 const connectDB = require('./db/connect');
 
@@ -14,6 +16,8 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 app.get('/', (req, res) => {
   res.send('<h1>File Upload Starter</h1>');
 });
+
+app.use('/api/v1/products' , productRouter)
 
 // middleware
 app.use(notFoundMiddleware);
